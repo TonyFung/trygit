ce44e917797d7c764556ff815b992f29191e73cf
TonyFung <378012325@qq.com>
2016-01-08 19:04:48 +0800

    test reset1

--- app/src/main/java/com/trygit/tonyfung/mygitapplication/A.java
+++ app/src/main/java/com/trygit/tonyfung/mygitapplication/A.java
@@ -1,7 +1,14 @@
 package com.trygit.tonyfung.mygitapplication;
 
+import android.util.Log;
+
 /**
  * Created by Administrator on 2016/1/8.
  */
 public class A {
+  public void log(){
+
+      Log.e("---A---","P1修改了这里");
+  }
+
 }
