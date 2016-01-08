1ce05fe5edea532d3bd9b93983c91ac6c0ec8743
TonyFung <378012325@qq.com>
2016-01-08 19:45:26 +0800

    我是p2我最牛逼

--- app/src/main/java/com/trygit/tonyfung/mygitapplication/A.java
+++ app/src/main/java/com/trygit/tonyfung/mygitapplication/A.java
@@ -1,7 +1,14 @@
 package com.trygit.tonyfung.mygitapplication;
 
+import android.util.Log;
+
 /**
  * Created by Administrator on 2016/1/8.
  */
 public class A {
+
+    public void p1finction(){
+        Log.e("p1---", "我是P2我最牛逼");
+    }
+
 }
